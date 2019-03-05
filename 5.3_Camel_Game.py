4cdc5bc3d2869c44268020772ee6c6d50fc0d53b
noahmarble <mablenoa@urbandaleschools.com>
2019-03-05 12:15:50 -0600

    done with #17 of camel game

--- 5.3_Camel_Game.py
+++ 5.3_Camel_Game.py
@@ -23,10 +23,10 @@ import random
 #variables
 done = False
 milestraveled = 0
-gas = 10             #your thirst
+gas = 6             #your thirst
 brokenparts = 0      #camel tierdness
 bigdude = -20        #natives
-
+tools = 3
 #command
 while not done:
     print("A. do MAINTENANCE on the jeep") #drink from canteen
@@ -39,20 +39,63 @@ while not done:
 
     #quit
     choice = input("What is your choice? ")
-    if choice.lower() == ("q") or choice.lower() == ("quit"):
+    if choice.lower() == "q" or choice.lower() == "quit":
         done = True
 
     #status
-    elif choice.lower() == ("e") or choice.lower() == ("status"):
+    elif choice.lower() == "e" or choice.lower() == "status":
         print("miles traveled:", milestraveled)
         print("gas:", gas)
         print("the big dude on the harley is", -bigdude, "miles behind you\n")
 
     #stop and refuel
-    elif choice.lower() == "d" or choice.lower() == ("STOP AND REFUEL"):
+    elif choice.lower() == "d" or choice.lower() == "stop and refuel":
         gas = 10
         randomseventofourteen = random.randrange(7,15)
         bigdude+= randomseventofourteen
+
+    #full speed
+    elif choice.lower() == "c" or choice.lower() == "full speed":
+        randomtetotwenty = random.randrange(10, 21)
+        milestraveled+= randomtetotwenty
+        gas-=1
+        randomonetothree = random.randrange(1, 4)
+        brokenparts+= randomonetothree
+        randomseventofourteen = random.randrange(7, 15)
+        bigdude += randomseventofourteen
+        print("miles traveled:", milestraveled)
+
+    #moderate speed
+    elif choice.lower() == "b" or choice.lower() == "moderate speed":
+        randomfivetotwelve = random.randrange(5, 12)
+        milestraveled += randomfivetotwelve
+        gas-= 1
+        brokenparts+=1
+        randomseventofourteen = random.randrange(7, 15)
+        bigdude += randomseventofourteen
+        print("miles traveled:", milestraveled)
+
+    #maitnance
+    elif choice.lower() == "a" or choice.lower() == "maitnence":
+        if tools >= 1
+            tools-= 1
+            broken parts = 0
+        else
+            print("error, no drinks")
+
+
+    #gas
+    elif gas <= 0:
+        print("you have ran out of gas")
+        break
+    elif gas <= 2:
+        print ("you are low on gas")
+
+    #broken parts
+
+
+
+
 
 
 
