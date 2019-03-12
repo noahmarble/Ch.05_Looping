79bab0542ea40c319030ddb02b8c0c8e0c635ac2
noahmarble <mablenoa@urbandaleschools.com>
2019-03-12 12:19:55 -0500

    re do #1, work on #5

--- practice.py
+++ practice.py
@@ -13,55 +13,16 @@ print("sum of numbers", sumofnumbers)
 positive = 0
 zero = 0
 negative = 0
+total = 0
 
-if number1 >= 1:
-    positive += 1
-elif number1 == 0:
-    zero += 1
-else:
-    negative += 1
-
-if number2 >= 1:
-    positive += 1
-elif number2 == 0:
-    zero += 1
-else:
-    negative += 1
-
-if number3 >= 1:
-    positive += 1
-elif number3 == 0:
-    zero += 1
-else:
-    negative += 1
-
-if number4 >= 1:
-    positive += 1
-elif number4 == 0:
-    zero += 1
-else:
-    negative += 1
-
-if number5 >= 1:
-    positive += 1
-elif number5 == 0:
-    zero += 1
-else:
-    negative += 1
-
-if number6 >= 1:
-    positive += 1
-elif number6 == 0:
-    zero += 1
-else:
-    negative += 1
-
-if number7 >= 1:
-    positive += 1
-elif number7 == 0:
-    zero += 1
-else:
-    negative += 1
+for i in range (7):
+    if number1 >= 1:
+        positive += 1
+    elif number1 == 0:
+        zero += 1
+    else:
+        negative += 1
+    i+=1
 print("number of positive:", positive)
 print("number of zeros:", zero)
 print("number f negatives:", negative)
